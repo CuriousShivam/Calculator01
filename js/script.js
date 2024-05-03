df21f52b2673fa3ecf857deb7f32e18ecc2969a0
SHIVAM KUMAR SAH <130750855+CuriousShivam@users.noreply.github.com>
2024-05-03 22:30:26 +0530

    commit 3 May
    calculator keyboard design,
    logic for solving given issue,
    pressing enter button functionality to solve problem

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
-sdlf
+function top0(x) {
+    document.getElementById(x).style.top = "0px";
+}
+
+function hide(className) {
+    document.getElementById(className).style.top = "-1000px";
+}
+
+
+function eval1() {
+    let expression = document.getElementById('enterno').value
+    // console.log(dd, dd.charCodeAt(0)) char code of 1st 
+    // console.log(typeof solution)
+    let solve = eval(expression)
+    document.getElementById('enterno').value = (expression.toString() + ' = ' + solve.toString())
+
+}
+
+// document.getElementById("enterno").addEventListener("keydown",(event) =>{
+//     if(event.key==='Enter'){
+//         console.log('enter is pressed')
+//     }
+// })
+
+function enterPressed(){
+    const textInput = document.getElementById('enterno')
+    document.getElementById('enterno').addEventListener('keydown',(event) => {
+        if(event.key === 'Enter'){
+            eval1()
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(document.getElementById('enterno').value)
+// - 45
+// * 42
+// / 47
+// ( 40
+// ) 41
+// ^ 94
+
+
