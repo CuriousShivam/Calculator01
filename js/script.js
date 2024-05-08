9adc2b49d5a443d7438af55d2081dcc1e6e9ad29
SHIVAM KUMAR SAH <130750855+CuriousShivam@users.noreply.github.com>
2024-05-08 21:14:45 +0530

    cards and calculator styling done
    options,login and signup cards ard aligned properly on page and calculator styled properly

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function top0(x) {
-    document.getElementById(x).style.top = "0px";
+    document.getElementById(x).style.top = "50%";
 }
 
 function hide(className) {
@@ -11,8 +11,8 @@ function eval1() {
     let expression = document.getElementById('enterno').value
     // console.log(dd, dd.charCodeAt(0)) char code of 1st 
     // console.log(typeof solution)
-    let solve = eval(expression)
-    document.getElementById('enterno').value = (expression.toString() + ' = ' + solve.toString())
+    let solve1 = eval(expression)
+    document.getElementById('enterno').value = (expression.toString() + ' = ' + solve1.toString())
 
 }
 
@@ -22,17 +22,39 @@ function eval1() {
 //     }
 // })
 
-function enterPressed(){
-    const textInput = document.getElementById('enterno')
-    document.getElementById('enterno').addEventListener('keydown',(event) => {
-        if(event.key === 'Enter'){
+function enterPressed() {
+    document.getElementById('enterno').addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
             eval1()
         }
     })
 }
 
 
+setInterval(function myFunction() {
+    if (window.screen.width < 300) {
+        // console.log('true')
+        document.getElementById('options').style.scale='.8'
+        document.getElementById('calculator').style.scale='.8'
+    }
+    else if(window.screen.width < 1000){
+        document.getElementById('options').style.scale='1'
+        document.getElementById('calculator').style.scale='1'
 
+    }
+    else {
+        // console.log(false)
+        document.getElementById('options').style.scale='1.5'
+        document.getElementById('calculator').style.scale='1.5'
+    }
+}, 10); 
+
+
+document.getElementById('cbtn').onclick = console.log('hello')
+
+function doWork(){
+console.log(    document.getElementById('cbtn').children.children.innerHTML)
+}
 
 
 
